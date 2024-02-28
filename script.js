a38aec6eabab7fef53af7f80be20d9f40e8c261d
okcabroad <okcabroad@gmail.com>
2024-02-29 00:04:20 +0900

    started editing the js to not include eval function

--- script.js
+++ script.js
@@ -7,14 +7,39 @@ function appendToDisplay(value){
 function clearDisplay(){
     display.value = " "
 }
+//three variables: first number, operator, second number 
+let firstNum = ""
+let operator = ""
+let secondNum = ""
 
-
-function calculate(){
-    try {
-        display.value = eval(display.value)
-    } catch (error){
-        display.value = 'Error';
+//function that takes an operator and 2 numbers 
+function operate(firstNum, operator, secondNum){
+    let result = "0";
+    //check if second number is zero for division
+    if (operator === '/' && secondNum === 0) {
+        return "Error: Cannot divide by zero";
     }
+    switch(operator) {
+        case '+':
+            result = firstNum + secondNum;
+            break;
+        case '-':
+            result = firstNum - secondNum;
+            break;
+        case '*':
+            result = firstNum * secondNum;
+            break;
+        case '/':
+            result = firstNum / secondNum;
+            break;
+        default:
+            return "Error..."
+    }
+    return result;
+
 }
+
+
+
 
 
